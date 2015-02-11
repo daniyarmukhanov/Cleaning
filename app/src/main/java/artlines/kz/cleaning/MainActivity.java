5731384764fcd2a06b6dd94b0118d91e62501117
daniyarmukhanov <daniyarmuslim@gmail.com>
2015-02-11 21:30:47 +0600

    main page

--- app/src/main/java/artlines/kz/cleaning/MainActivity.java
+++ app/src/main/java/artlines/kz/cleaning/MainActivity.java
@@ -1,39 +1,156 @@
 package artlines.kz.cleaning;
 
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private static int currentMenuPosition = -1;
+
+    private SlidingMenu menu;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_ACTION_BAR);
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+        final SlidingMenu menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setFadeDegree(0.35f);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_WINDOW);
+        menu.setMenu(R.layout.sidemenu);
+        menu.setBackgroundColor(getResources().getColor(R.color.sliding_menu_back_color));
+        menu.setBehindWidthRes(R.dimen.slidingmenu_behind_width);
+        menu.setSelectorDrawable(R.drawable.sidemenu_items_background);
+
+
+        ((ListView) findViewById(R.id.sidemenu)).setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                menuToggle();
+
+                if (currentMenuPosition != position)
+                    changeFragment(position);
+
+                currentMenuPosition = position;
+            }
+        });
+
+        if (currentMenuPosition != -1) {
+            ((ListView) findViewById(R.id.sidemenu)).setItemChecked(currentMenuPosition, true);
+        }
+
+        String[] items = {"Авторизация","FAQ","О Компании","Контакты"};
+        ((ListView) findViewById(R.id.sidemenu)).setAdapter(
+                new ArrayAdapter<Object>(
+                        this,
+                        R.layout.sidemenu_item,
+                        R.id.text,
+                        items
+                )
+        );
+
+        this.menu = menu;
+
+        ImageView menuicon=(ImageView)findViewById(R.id.sidebar);
+        menuicon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                menuToggle();
+            }
+        });
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if(menu.isMenuShowing()){
+                menu.toggle(true);
+                return false;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    private void changeFragment(int position) {
+        switch (position) {
+            case 0:
+                //showFragment(new MainFragment());
+                break;
+            case 1:
+               // showFragment(new ChannelsFragment());
+                break;
+            case 2:
+              //  showFragment(new NewsFragment("bookmark"));
+                break;
+            case 3:
+             //   showFragment(new TopicsFragment());
+                break;
+        }
+    }
+
+    private void showFragment(Fragment currentFragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, currentFragment)
+                .commit();
+    }
+
+    public SlidingMenu getMenu() {
+        return menu;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
+//        switch (item.getItemId()) {
+//            case android.R.id.home:
+//                menuToggle();
+//                return true;
+//
+//
+//
+//        }
         return super.onOptionsItemSelected(item);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void menuToggle(){
+        if(menu.isMenuShowing())
+            menu.showContent();
+        else
+            menu.showMenu();
+    }
+
 }
